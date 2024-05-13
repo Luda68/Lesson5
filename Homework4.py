e6e982370efd5a87c44600f088a0bd22cf959715
Luda68 <pril3pskay@yandex.ru>
2024-05-13 15:58:49 +0500

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,8 @@
+immutable_var=("Day",1,2,False)
+print(immutable_var)
+#immutable_var[0]=5
+#print(immutable_var) #кортеж не поддерживает изменение элементов
+mutable_list= ["one", 3, 5, "two"]
+print(mutable_list)
+mutable_list[1]="apple"
+print(mutable_list)
